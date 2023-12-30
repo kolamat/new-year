bc20d3391990ba6cd5d8e87264059358342ec1b1
kolamat <matanmidupe004@gmail.com>
2023-12-30 08:27:07 +0100

    Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 "use strict";
 
 let canvas, width, height, ctx;
+let fireworks = [];
+let particles = [];
+
 function setup() {
   canvas = document.getElementById("canvas");
   setSize(canvas);
   ctx = canvas.getcontext("2d");
   ctx.fillstyle = "000000";
-  ctx.fillRect;
+  ctx.fillRect(0, 0, width, height);
+  fireworks.push(new Firewors(math.random() * (width - 200) + 100));
+  window.addEventListener("resize", windowResized);
+  document.addEventListener("click", onclick);
 }
+
+setTimeout(setup, 1);
