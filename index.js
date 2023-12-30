3499d4e74d01e90feaa4199dc22379d756895d70
kolamat <matanmidupe004@gmail.com>
2023-12-30 08:18:29 +0100

    Changes to be committed: 	modified:   index.html 	modified:   index.js 	modified:   style.css

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let canvas, width, height, ctx;
+function setup() {
+  canvas = document.getElementById("canvas");
+  setSize(canvas);
+  ctx = canvas.getcontext("2d");
+  ctx.fillstyle = "000000";
+  ctx.fillRect;
+}
