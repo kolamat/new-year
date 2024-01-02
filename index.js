8c5c0946d6898cd6fa8ceb961aba7b62686af696
kolamat <matanmidupe004@gmail.com>
2024-01-02 21:15:39 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function addElement() {
 
   // add the text node to the newly created div
   newh1.appendChild(newContent);
-  newh1.appendChild(secondLine);
+  oldH1.appendChild(secondLine);
 
   // add the newly created element and its content into the DOM
   const currentH1 = document.getElementById("h1");
