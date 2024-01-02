8f70f67630063c23c1ad06560d936e7c4212e807
kolamat <matanmidupe004@gmail.com>
2024-01-02 21:13:26 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -8,11 +8,16 @@ function addElement() {
   const newContent = document.createTextNode(
     "Hi there my name is Kolade matanmi an a frontend engineer!"
   );
+  setTimeout(() => {
+    let secondLine = document.createTextNode("and welcome to my page");
+  }, 2000);
 
   // add the text node to the newly created div
   newh1.appendChild(newContent);
+  newh1.appendChild(secondLine);
 
   // add the newly created element and its content into the DOM
   const currentH1 = document.getElementById("h1");
+  const secondH1 = document.getElementById("h1");
   document.body.insertBefore(newh1, currentH1);
 }
